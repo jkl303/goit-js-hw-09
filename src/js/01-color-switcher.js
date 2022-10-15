f524c499c126deebebd82ede5e9dc47c1d40c16b
jkl303 <303vladzoz@gmail.com>
2022-10-15 09:40:08 +0200

    added intId

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,7 +9,7 @@ function changeBodyColor() {
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-intId = setInterval(changeBodyColor, 1000);
+const intId = setInterval(changeBodyColor, 1000);
 refs.stopBtn.disabled = true;
 refs.startBtn.addEventListener('click', () => {
     intId;
