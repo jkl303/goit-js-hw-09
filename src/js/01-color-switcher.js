9a14d33775e31a3ee4851d409f82cf5b55b14aab
jkl303 <303vladzoz@gmail.com>
2022-10-15 08:01:16 +0200

    completed

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+const refs = {
+    body : document.querySelector('body'),
+    startBtn : document.querySelector('[data-start]'),
+    stopBtn : document.querySelector('[data-stop]')
+}
+function changeBodyColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+refs.stopBtn.disabled = true;
+refs.startBtn.addEventListener('click', () => {
+    intId = setInterval(changeBodyColor, 1000);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+});
+refs.stopBtn.addEventListener('click', () => {
+    clearInterval(intId);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+});
+
